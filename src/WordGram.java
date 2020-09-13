6a8ec3fc2bb5f2b1863ea20892c690ca0fed197a
Cate Schick <catherine.schick@duke.edu>
2020-09-13 16:29:05 -0400

    Finished some code for P2

--- src/WordGram.java
+++ src/WordGram.java
@@ -3,7 +3,7 @@
  * A WordGram represents a sequence of strings
  * just as a String represents a sequence of characters
  * 
- * @author YOUR NAME HERE
+ * @author Cate Schick
  *
  */
 public class WordGram {
@@ -21,10 +21,12 @@ public class WordGram {
 	 */
 	public WordGram(String[] source, int start, int size) {
 		myWords = new String[size];
-		myToString = null;
-		myHash = 0;
+		myToString = myWords.toString();
+		myHash = myToString.hashCode();
 
-		// TODO: initialize all instance variable
+		// TODO: initialize all instance variables
+		// copy array
+		System.arraycopy(source, start, myWords, 0, size);
 	}
 
 	/**
@@ -40,17 +42,17 @@ public class WordGram {
 	}
 
 	/**
-	 * Complete this comment
+	 * Returns the length of myWords
 	 * @return
 	 */
 	public int length(){
 		// TODO: change this
-		return 0;
+		return myWords.length;
 	}
 
-
 	/**
-	 * Complete appropriate comment here
+	 * Return true when parameter passed is a WordGram object with the same
+	 * strings in the same order as this object
 	 * @param o
 	 * @return
 	 */
@@ -60,10 +62,32 @@ public class WordGram {
 			return false;
 		}
 		// TODO: Complete this method
+		// if o is a wordgram, which it has to be to get here
+		// in the code, it can be written as:
+		WordGram other = (WordGram) o;
+
+		// the arrays need to have the same length
+		if (this.length() != other.length()) {
+			return false;
+		}
+
+		// the arrays need to have the same words
+		for (int i = 0; i < this.length(); i++) {
+			if (!this.wordAt(i).equals(other.wordAt(i))) {
+				return false;
+			}
+			else {
+				return true;
+			}
+		}
 
 		return true;
 	}
 
+	/**
+	 * returns an integer value based on all the strings
+	 * @return
+	 */
 	@Override
 	public int hashCode(){
 		// TODO: complete this method: assign to myHash as needed
@@ -72,20 +96,35 @@ public class WordGram {
 	
 
 	/**
-	 * Create and complete this comment
+	 * Creates and returns a new WordGram object with k entries
+	 * whose first k-1 entries are the same as the last k-1 entries
 	 * @param last is last String of returned WordGram
 	 * @return
 	 */
 	public WordGram shiftAdd(String last) {
 		WordGram wg = new WordGram(myWords,0,myWords.length);
 		// TODO: Complete this method
+		String[] words = new String[this.length()];
+		myWords = words;
 
-		return wg;
+		for (int i = 0; i < myWords.length-1; i++) {
+			words[i] = this.wordAt(i+1);
+		}
+
+		words[words.length-1] = last;
+
+		WordGram ret = new WordGram(words, 0, words.length);
+
+		return ret;
 	}
 
 	@Override
 	public String toString(){
 		// TODO: Complete this method, assign to myToString as needed
+		for (int i = 0; i < myWords.length; i++) {
+			myToString = myToString + (myWords[i]);
+			myToString = myToString + (" ");
+		}
 		return myToString;
 	}
 }
